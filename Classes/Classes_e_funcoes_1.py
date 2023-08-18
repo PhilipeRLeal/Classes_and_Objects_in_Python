901c8943025ad63807f78a322a4c7c9911a9c578
Philipe Riskalla Leal <32545466+PhilipeRLeal@users.noreply.github.com>
2023-08-18 20:19:30 -0300

    Update Classes_e_funcoes_1.py

--- Classes/Classes_e_funcoes_1.py
+++ Classes/Classes_e_funcoes_1.py
@@ -5,6 +5,8 @@ Created on Tue Feb 20 11:29:24 2018
 @author: Philipe Leal
 """
 
+import math
+
 class Point: # definicao de classe
         
     def __init__(self, x=0, y=0): # funcao de iinicializacao da classe criada
@@ -15,7 +17,6 @@ class Point: # definicao de classe
         return self.x==other.x and self.y == other.y
         
     def distancia_da_origem(self): # propriedade da Classe Point
-        import math
                                         # math.hypot fornece a distância euclidiana do ponto em relacao ao ponto (0,0)
         return math.hypot(self.x, self.y) # retorna a distância da origem
         
@@ -33,18 +34,11 @@ class Circle(Point):
     def __init__(self, radius, x=0, y=0):
         super().__init__(x,y)
         self.radius = radius
-    
-    def edge_distance_from_origin(self):
-        
-        return (self.distancia_da_origem() - self.radius)
-    
+            
     def area(self):
-        import math
-        
         return (math.pi * (self.radius**2))
     
-    def circumference(self):
-        import math
+    def circumferencia(self):
         return 2 * math.pi * self.radius
     
     def __eq__(self, other):
@@ -52,6 +46,10 @@ class Circle(Point):
     
     def __str__(self):
         return (repr(self))
+
+    def contem(self, point: Point):
+        check = point.distancia_da_origem() <= self.circumference()
+        return check
     
     def __repr__(self): # funcao que permite ao usuario checar qual eh o tipo de objeto, além de retornar uma string com o tipo do objeto
         """ Esta função é utilizada para retornar uma string com o Tipo da classe definida e sua instância.
